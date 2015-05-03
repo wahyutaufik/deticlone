b565d500dd0beb425ad1e51e9aade2ec190de586
Wahyu Taufik <wahyutaufik37@gmail.com>
2015-05-03 22:32:26 +0700

    Update link on header

--- head.php
+++ head.php
@@ -1,7 +1,7 @@
 <div class="header_top" id="top">
     <div class="head">
         <div style="z-index:2;">
-            <a href="http://www.detik.com" style="border:0;">
+            <a href="index.php" style="border:0;">
                 <img alt="detikcom" class="logodetik" src="layout/img/logodetikcom.png">
             </a>
         </div>
